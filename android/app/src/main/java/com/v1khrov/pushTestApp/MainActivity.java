6a3f6e1cf9634e777520d1fd851e44f55dd069e5
Dmitry Vikhrov <d.vikhrov@lcgs.ru>
2025-07-14 13:51:59 +0300

    add capacitor

---
+++ android/app/src/main/java/com/v1khrov/pushTestApp/MainActivity.java
@@ -0,0 +1,5 @@
+package com.v1khrov.pushTestApp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.v1khrov.pushTestApp',
+  appName: 'PushTestApp',
+  webDir: 'www'
+};
+
+export default config;
